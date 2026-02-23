22ccbfaade96cc1f5dd7d481b6e1a1a0f202338b
Rajratnanikalje <nikaljerajratna54@gmail.com>
2026-02-23 14:04:57 +0530

    promise

---
+++ Promise/index.js
@@ -0,0 +1,177 @@
+// let promise = new Promise(function(res,rej){
+//     let success = true;
+//     if(success){
+//         res("task completed")
+//     }else{
+//         rej("task failed")
+//     }
+// })
+// console.log(promise);
+
+// let promise = new Promise(function(res,rej){
+//     let success = false;
+//     if(success){
+//         res("task completed")
+//     }else{
+//         rej("task failed")
+//     }
+// })
+// console.log(promise);
+
+// let promise = new Promise(function(res,rej){
+//     let success = true;
+//     if(success){
+//         res("task completed")
+//     }else{
+//         rej("task failed")
+//     }
+// })
+// console.log(promise);
+
+// let promise = new Promise(function(res,rej){
+//     let success = false;
+//     if(success){
+//         res("task completed")
+//     }else{
+//         rej("task failed")
+//     }
+// })
+// console.log(promise);
+
+
+// let promise = new Promise(function(res,rej){
+//     let success = true;
+//     setTimeout(()=>{
+//         if(success){
+//             res("task complete")
+//         }else{
+//             rej("task failed")
+//         }
+//     },5000)
+// })
+
+// promise.then(function(res){
+//     console.log(res);
+    
+// })
+
+// promise.catch(function(rej){
+//     console.log(rej);
+    
+// })
+
+// let promise = new Promise(function(res,rej){
+//     let success = false;
+//     setTimeout(()=>{
+//          if(success){
+//         res("done")
+//     }else{
+//         rej("failed")
+//     }
+//     },5000)
+
+// })
+
+// promise.then(function(res){
+//     console.log(res);
+    
+// })
+
+// promise.catch(function(rej){
+//     console.log(rej);
+    
+// })
+
+// let promise = new Promise(function(res,rej){
+//     let success = true;
+//     setTimeout(() => {
+//         if(success){
+//             res("done")
+//         }else{
+//             rej("failed")
+//         }
+//     }, 5000);
+// })
+
+// promise.then(function(res){
+//     console.log(res);
+    
+// })
+
+// promise.catch(function(rej){
+//     console.log(rej);
+    
+// })
+
+// let promise = new Promise(function(res,rej){
+//     let success = false;
+//     setTimeout(()=>{
+//          if(success){
+//         res("done")
+//     }else{
+//         rej("failed")
+//     }
+//     },5000)
+   
+// })
+
+// promise.then(function(res){
+//     console.log(res);
+    
+// })
+
+// promise.catch(function(rej){
+//     console.log(rej);
+    
+// })
+
+
+// let promise = new Promise(function(res,rej){
+//     let success = true;
+
+//     setTimeout(()=>{
+//  if(success){
+//         res("done")
+//     }else{
+//         rej("failed")
+//     }
+//     },5000)
+   
+// })
+
+// promise.then(function(res){
+//     console.log(res);
+    
+// })
+
+// promise.catch(function(rej){
+//     console.log(rej);
+    
+// })
+
+let promise = new Promise(function(res,rej){
+    let success = false;
+    setTimeout(()=>{
+         if(success){
+        res("done")
+    }else{
+        rej("failed")
+    }
+    },5000)
+   
+})
+
+promise.then(function(res){
+    console.log(res);
+    
+})
+
+promise.catch(function(rej){
+    console.log(rej);
+    
+})
+
+
+
+
+
