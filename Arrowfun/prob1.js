cc6cefe01f8c18266ded60b475eefac0c5758563
Rajratnanikalje <nikaljerajratna54@gmail.com>
2026-02-24 12:55:55 +0530

    add

--- Arrowfun/prob1.js
+++ Arrowfun/prob1.js
@@ -1,3 +1,4 @@
 // Arrow Functions — 25 Interview Questions
 //  Conceptual
 // What is an arrow function in JavaScript?
+// ans:- An arrow function in JavaScript is a shorter and cleaner way to write functions, introduced in ES6 (ECMAScript 2015).
